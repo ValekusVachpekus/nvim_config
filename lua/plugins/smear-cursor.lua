8b4f20691214f36e052e306aeeb5415fb530612b
L0kus <sasihuipendos@gmail.com>
2026-02-16 13:25:12 +0300

    New plugin for cursor

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,10 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    -- Настройки по вкусу (можно оставить пустыми)
+    smear_between_buffers = true,
+    smear_between_neighbor_lines = true,
+    stiffness = 0.5,
+    trailing_stiffness = 0.5,
+  },
+}
