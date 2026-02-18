c5d3088d8df2c115ae6ea62e13193d622eb6a0aa
L0kus <sasihuipendos@gmail.com>
2026-02-18 13:09:17 +0300

    Few changes

--- init.lua
+++ init.lua
@@ -5,3 +5,4 @@ vim.opt.tabstop = 4 -- 1 таб = 4 пробела
 vim.opt.shiftwidth = 4 -- Автоотступ = 4 пробела
 vim.opt.expandtab = true -- Преобразовывать табы в пробелы
 vim.opt.autoindent = true -- Сохранять отступ на новой строке
+-- vim.opt.guicursor = "" -- Полностью доверяем управление курсором терминалу
--- lua/plugins/smear-cursor.lua
+++ lua/plugins/smear-cursor.lua
@@ -3,7 +3,7 @@ return {
   opts = {
     -- Настройки по вкусу (можно оставить пустыми)
     smear_between_buffers = true,
-    smear_between_neighbor_lines = true,
+    smear_between_neighbor_lines = false,
     stiffness = 0.5,
     trailing_stiffness = 0.5,
   },
