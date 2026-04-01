08f35c8acd0d91d3ad5d385d5af4bb8d3d8c0557
L0kus <sasihuipendos@gmail.com>
2026-04-01 13:34:58 +0300

    feat: configure wrap

--- init.lua
+++ init.lua
@@ -6,3 +6,8 @@ vim.opt.shiftwidth = 4 -- Автоотступ = 4 пробела
 vim.opt.expandtab = true -- Преобразовывать табы в пробелы
 vim.opt.autoindent = true -- Сохранять отступ на новой строке
 -- vim.opt.guicursor = "" -- Полностью доверяем управление курсором терминалу
+
+-- Перенос длинных строк
+vim.opt.wrap = true -- Включить визуальный перенос строк
+vim.opt.linebreak = true -- Переносить по словам, а не по символам
+vim.opt.breakindent = true -- Сохранять отступ при переносе
